287bcd1ffcc274bf4a5f385a1e73b4e5ab2b16aa
phananhson24101995 <phananhson24101995@gmail.com>
2019-01-23 13:01:59 +0700

    COMMIT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import {MatTableModule} from '@angular/material';
+import {MatInputModule} from '@angular/material/input';
+import { MyTableComponent } from './my-table/my-table.component';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    MyTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatTableModule,
+    MatInputModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
